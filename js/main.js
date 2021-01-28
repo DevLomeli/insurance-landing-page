13ff8345a75786f8b61c4b0d9d3cf30397b98d26
Fernando Lomeli <fercho0.fpl@gmail.com>
2021-01-28 15:19:14 -0600

    Some fixes in mobile approach

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 const hamburguerIcon = document.querySelector(".header__hamburguerIcon");
 const navMenuMobile = document.querySelector(".mobileMenu");
 const body = document.querySelector("body");
+
 var toggle = false;
+
 hamburguerIcon.addEventListener("click", () => {
+
   if (!toggle) {
+
     toggle = true;
     hamburguerIcon.src = "./images/icon-close.svg";
     navMenuMobile.classList.add("mobileMenu--show");
     body.style.position = "fixed";
+
   } else {
+
     toggle = false;
     hamburguerIcon.src = "./images/icon-hamburger.svg";
     navMenuMobile.classList.remove("mobileMenu--show");
     body.style.removeProperty("position");
+    
   }
 });
